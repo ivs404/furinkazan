957c0369b591d8a5f12de1a196545e00870b725d
ivs404 <ivs404@yandex.ru>
2024-10-12 02:20:58 +0500

    upd 123

--- bot.py
+++ bot.py
@@ -16,27 +16,34 @@ bot = telegram.Bot(token=BOT_TOKEN)
 # Настройка Instaloader
 L = instaloader.Instaloader()
 
-# Ваши учетные данные Instagram
-instagram_username = 'martin_06_10_24'  # Замените на ваш логин
-instagram_password = 'g8bRncEvHdxfMKY'  # Замените на ваш пароль
+# Укажите ваш логин и пароль Instagram
+# Вместо этого вы можете использовать cookies
+# Для использования cookies:
+# L.load_session_from_file("your_username")  # Если у вас есть сохраненная сессия
 
-# Вход в Instagram
-L.login(instagram_username, instagram_password)
-
-# Укажите Instagram аккаунт, из которого нужно загружать видео
-username = "tvoretzz"
-
-# Получаем профиль
+# Или войдите в Instagram через cookies
+cookies_file = 'cookies.json'  # Укажите путь к файлу с cookies
 try:
-    profile = instaloader.Profile.from_username(L.context, username)
+    L.load_session_from_file("martin_06_10_24")  # Замените на ваш логин Instagram
+except FileNotFoundError:
+    logger.info("Файл с сессией не найден. Проверьте наличие cookies.")
+    exit(1)
+
+# Имя пользователя профиля, из которого хотите получать видео
+profile_username = 'tvoretzz'  # Замените на желаемое имя пользователя
+
+try:
+    # Получаем профиль
+    profile = instaloader.Profile.from_username(L.context, profile_username)
 except instaloader.exceptions.ProfileNotExistsException:
-    logger.error(f'Профиль "{username}" не существует.')
-    exit()
+    logger.error(f"Профиль {profile_username} не существует.")
+    exit(1)
 
 # Загрузка новых видео
 def download_and_send_videos():
     for post in profile.get_posts():
         if post.typename == 'GraphVideo':
+            logger.info(f"Загрузка видео: {post.shortcode}")
             # Загрузить видео
             video_url = post.video_url
             video_filename = f"{post.mediaid}.mp4"
@@ -48,6 +55,7 @@ def download_and_send_videos():
 
             # Удалить файл после отправки
             os.remove(video_filename)
+            logger.info(f"Видео отправлено в Telegram: {post.shortcode}")
 
 if __name__ == "__main__":
     download_and_send_videos()
